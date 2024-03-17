bafac5ab7a0e463535740ee616eca9aa244eb035
ascreation-git <ascreation.ent@gmail.com>
2024-03-17 18:31:59 +0900

    Edit

--- application/application.js
+++ application/application.js
@@ -7,10 +7,11 @@ const application = express()
 // App View Setting
 application.set('views', './sources/application-views')
 application.set('view engine', 'ejs')
+application.use(express.static(`${__dirname}/sources/public`)) // Register Middleware
 
 // Routing
 const home = require('./sources/routes/home')
-application.use('/', home) // Use > Method to register middleware
+application.use('/', home) // Use > Method to Register Middleware
 
 // Export
 module.exports = application
