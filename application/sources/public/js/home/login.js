3a9b161be2f30c2d2cde179cc2aed7fe52833053
ascreation-git <ascreation.ent@gmail.com>
2024-03-17 19:02:07 +0900

    Create Fecth to Request Login

--- application/sources/public/js/home/login.js
+++ application/sources/public/js/home/login.js
@@ -1,1 +1,22 @@
 'use strict'
+
+const id = document.querySelector('.login-form__id')
+const password = document.querySelector('.login-form__password')
+const loginButton = document.querySelector('.login-form__submit')
+
+loginButton.addEventListener('click', login)
+
+function login() {
+    const req = {
+        id: id.value,
+        password: password.value
+    }
+
+    fetch('/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(req)
+    })
+}
